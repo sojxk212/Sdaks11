bbd0c946145da0c077609dc006a09694622ceded
الكنج حمودي الاسطوره <mogsnfknsgqns@gmail.com>
2024-09-05 14:21:23 +0300

    Create Sdjsiwoks4.sh

---
+++ Sdjsiwoks4.sh
@@ -0,0 +1,72 @@
+# A tool by nitro from www.noob-hackers.com
+# Be a unique person not copy cat
+# Colours
+#-----------------
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+echo " " 
+echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo " " 
+echo -e "$red                       ▶Coded by$grn NITRO$red◀$rset"
+echo -e "$red                      ⫸$ylo  Noob Hackers$red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [Infect them with fun]$rset"
+echo " "
+echo " "
+}
+baner1(){
+clear
+echo -e '\e[93m               
+
+
+/\\  ______________ \\
+      /::\\ \\ZZZZZZZZZZZZ/\\ \\
+     /:/\\.\\ \\        /:/\\:\\ \\
+    /:/Z/\\:\\ \\      /:/Z/\\:\\ \\
+   /:/Z/__\\:\\ \\____/:/Z/  \\:\\ \\
+  /:/Z/____\\:\\ \\___\\/Z/    \\:\\ \\
+  \\:\\ \\ZZZZZ\\:\\ \\ZZ/\\ \\     \\:\\ \\
+   \\:\\ \\     \\:\\ \\ \\:\\ \\     \\:\\ \\
+    \\:\\ \\     \\:\\ \\_\\;\\_\\_____\\;\\ \\
+     \\:\\ \\     \\:\\_________________\\
+      \\:\\ \\    /:/ZZZZZZZZZZZZZZZZZ/
+       \\:\\ \\  /:/Z/    \\:\\ \\  /:/Z/
+        \\:\\ \\/:/Z/      \\:\\ \\/:/Z/
+         \\:\\/:/Z/________\\;\\/:/Z/
+          \\::/Z/_______itz__\\/Z/
+           \\/ZZZZZZZZZZZZZZZZZ/
+           
+'
+
+echo -e "$red                       ▶الهكر اليماني $grn حمودي $red◀$rset"
+echo -e "$red                      ⫸$ylo  الهكرز الكنج حمودي الاسطورة $red ⫷$rset"
+echo 
+# Functions
+echo -e "$red                    [ الكنج حمودي الاسطورة للبرمجه و الشروحات والتعليم ]$rset"
+echo " "
+echo " "
+}
+menu(){
+echo -e "$grn             ＞＞＞＞>>>＞$ylo [اده عربيه دعس الارقام الواتس اب المطور الكنج حمودي الاسطورة ]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+echo -e "$red                        ➡$cyan [\e[92m1\e[91m] ابلاغ عن الرقم اباحي"
+echo -e "$red                        ➡$cyan [\e[92m2\e[96m] ابلاغ عن الرقم ارهابي "
+echo -e "$red                        ➡$cyan [\e[92m3\e[97m] ابلاغ عن الرقم "
+echo -e "$red                        ➡$cyan [\e[92m4\e[92m] ابلاغ بقوه الف "
+echo -e "$red   
